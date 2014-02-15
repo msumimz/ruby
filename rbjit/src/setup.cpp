aac0d29125afd41ef123287b8bad62fbdafde537
msumimz <msumimz@users.noreply.github.com>
2014-02-16 08:06:26 +0900

    add Object#dumptree

--- rbjit/src/setup.cpp
+++ rbjit/src/setup.cpp
@@ -1,9 +1,18 @@
+extern "C" {
 #include "ruby/ruby.h"
+#include "method.h" // rb_method_entry_t, rb_method_definition_t
+#include "vm_core.h" // rb_iseq_t
+#include "node.h" // rb_parser_dump_tree
+}
+
 #include "rbjit/common.h"
 
 extern "C" {
 void Init_rbjit();
 }
+
+////////////////////////////////////////////////////////////
+// Development tools
 
 static VALUE
 debugbreak(int argc, VALUE *argv, VALUE obj)
@@ -12,9 +21,28 @@ debugbreak(int argc, VALUE *argv, VALUE obj)
   return Qnil;
 }
 
+static VALUE
+dumptree(VALUE self, VALUE cls, VALUE methodName)
+{
+  rb_method_entry_t* me = rb_method_entry(cls, SYM2ID(methodName), 0);
+
+  rb_method_definition_t* def = me->def;
+  if (def->type != VM_METHOD_TYPE_ISEQ) {
+    rb_raise(rb_eArgError, "method does not have iseq")
+  }
+
+  NODE* node = def->body.iseq->node;
+
+  rb_io_write(rb_stdout, rb_parser_dump_tree(node, 0));
+  rb_io_flush(rb_stdout);
+
+  return Qnil;
+}
+
 void
 Init_rbjit()
 {
   rb_define_method(rb_cObject, "debugbreak", (VALUE (*)(...))debugbreak, 0);
+  rb_define_method(rb_cObject, "dumptree", (VALUE (*)(...))dumptree, 2);
 }
 
