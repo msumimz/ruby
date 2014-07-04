c67d76689cc63bc398f8e93a6ed548f65d217283
msumimz <msumimz@users.noreply.github.com>
2014-07-05 01:56:39 +0900

    Update build scripts to build the full-fledged ruby installation

--- win32/rbjit_build.mk
+++ win32/rbjit_build.mk
@@ -1,3 +1,7 @@
 {$(rbjit_srcdir)}.cpp.obj:
 	$(ECHO) compiling $(<:\=/)
 	$(Q) $(CC) $(CFLAGS) $(XCFLAGS) $(CPPFLAGS) $(RBJITFLAGS) $(COUTFLAG)$@ -c -Tp $(<:\=/)
+
+{$(rbjit_primdir)}.rc.res:
+	$(ECHO) compiling $(<:\=/)
+	$(Q) $(RC) $(RCFLAGS) -I. -I$(<D) $(RFLAGS) -fo$@ $(<:\=/)
--- win32/rbjit_macros.mk
+++ win32/rbjit_macros.mk
@@ -1,5 +1,7 @@
-RBJIT_CPPFLAGS = /wd4355 /I./rbjit /I./rbjit/include /I$(LLVM_PREFIX)/include /DNDEBUG /D_SCL_SECURE_NO_WARNINGS=1 /DRBJIT_ENABLED /GR /EHsc /D_SECURE_SCL=0 /D_HAS_ITERATOR_DEBUGGING=0 /D_ITERATOR_DEBUG_LEVEL=0 /Ob2 /Oi /GL
+RBJIT_CPPFLAGS = /wd4355 /I./rbjit /I./rbjit/include /I$(LLVM_PREFIX)/include /DNDEBUG /D_SCL_SECURE_NO_WARNINGS=1 /DRBJIT_ENABLED /GR /EHsc /D_SECURE_SCL=0 /D_HAS_ITERATOR_DEBUGGING=0 /D_ITERATOR_DEBUG_LEVEL=0 /Ob2 /Oi
 
-RBJIT_LDFALGS = /libpath:$(LLVM_PREFIX)/lib /LTCG /DYNAMICBASE /NXCOMPAT /SAFESEH
+RBJIT_LDFALGS = /libpath:$(LLVM_PREFIX)/lib /DYNAMICBASE /NXCOMPAT /SAFESEH
 
 RBJIT_LIBS = LLVMipo.lib LLVMBitReader.lib LLVMX86Disassembler.lib LLVMX86AsmParser.lib LLVMX86CodeGen.lib LLVMSelectionDAG.lib LLVMAsmPrinter.lib LLVMMCParser.lib LLVMX86Desc.lib LLVMX86Info.lib LLVMX86AsmPrinter.lib LLVMX86Utils.lib LLVMJIT.lib LLVMRuntimeDyld.lib LLVMExecutionEngine.lib LLVMCodeGen.lib LLVMObjCARCOpts.lib LLVMScalarOpts.lib LLVMInstCombine.lib LLVMTransformUtils.lib LLVMipa.lib LLVMAnalysis.lib LLVMTarget.lib LLVMMC.lib LLVMObject.lib LLVMCore.lib LLVMSupport.lib
+
+RBJIT_ARFLAGS =
