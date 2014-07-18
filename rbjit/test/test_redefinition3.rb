9ec6785d06a5813ecf6a82c3ac41766ba38ea8f0
msumimz <msumimz@users.noreply.github.com>
2014-07-18 11:23:10 +0900

    Add test/test_redefinition3.rb

---
+++ rbjit/test/test_redefinition3.rb
@@ -0,0 +1,17 @@
+load File.expand_path("assertions.rb", File.dirname(__FILE__))
+
+alias foobar eval
+
+def inlined
+  3
+end
+
+def m(s)
+  a = inlined
+  foobar s
+  a + inlined
+end
+
+precompile Object, :m
+
+assert(m("def inlined; 5; end") == 8)
