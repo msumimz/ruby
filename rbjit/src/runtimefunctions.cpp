ddb0740d17efdb216349682664c8bb86e3e60631
msumimz <msumimz@users.noreply.github.com>
2014-07-28 01:52:56 +0900

    Implement runtime functions for literal creation

--- rbjit/src/runtimefunctions.cpp
+++ rbjit/src/runtimefunctions.cpp
@@ -14,6 +14,9 @@ vm_get_ev_const(rb_thread_t *th, const rb_iseq_t *iseq,
 
 RBJIT_NAMESPACE_BEGIN
 
+////////////////////////////////////////////////////////////
+// Method call
+
 rb_method_entry_t*
 rbjit_lookupMethod(VALUE receiver, ID methodName)
 {
@@ -41,12 +44,113 @@ rbjit_callMethod(rb_method_entry_t* me, int argc, VALUE receiver, ...)
   return result;
 }
 
+////////////////////////////////////////////////////////////
+// Constant lookup
+
 VALUE
 rbjit_findConstant(VALUE baseClass, ID name, void* iseq)
 {
   // Call MRI's standard constant lookup function.
   // This function involves autoloading and exception raising.
   return vm_get_ev_const(GET_THREAD(), (const rb_iseq_t*)iseq, baseClass, name, 0);
+}
+
+////////////////////////////////////////////////////////////
+// Array
+
+VALUE
+rbjit_createArray(int count, ...)
+{
+  va_list list;
+  va_start(list, count);
+  VALUE* firstArg = &(va_arg(list, VALUE));
+
+  return rb_ary_new4(count, firstArg);
+}
+
+VALUE
+rbjit_convertToArray(VALUE obj)
+{
+  VALUE result = rb_check_convert_type(obj, T_ARRAY, "Array", "to_a");
+  if (NIL_P(obj)) {
+    result = rb_ary_new3(1, obj);
+  }
+  return obj;
+}
+
+VALUE
+rbjit_concatenateArrays(int count, ...)
+{
+  va_list list;
+  va_start(list, count);
+  VALUE result = rb_ary_dup(va_arg(list, VALUE));
+
+  for (int i = 1; i < count; ++i) {
+    VALUE a = va_arg(list, VALUE);
+    rb_ary_concat(result, a);
+  }
+
+  return result;
+}
+
+////////////////////////////////////////////////////////////
+// Range
+
+VALUE
+rbjit_createRange(VALUE low, VALUE high, int exclusive)
+{
+  return rb_range_new(low, high, exclusive);
+}
+
+////////////////////////////////////////////////////////////
+// String
+
+VALUE
+rbjit_duplicateString(VALUE s)
+{
+  return rb_str_dup(s);
+}
+
+VALUE
+rbjit_convertToString(VALUE obj)
+{
+  return rb_obj_as_string(obj);
+}
+
+VALUE
+rbjit_concatenateString(int count, ...)
+{
+  va_list list;
+  va_start(list, count);
+  VALUE result = rb_str_dup(va_arg(list, VALUE));
+
+  for (int i = 1; i < count; ++i) {
+    VALUE s = va_arg(list, VALUE);
+    rb_str_append(result, s);
+  }
+
+  return result;
+}
+
+////////////////////////////////////////////////////////////
+// Hash
+
+VALUE
+rbjit_createHash(int count, ...)
+{
+  assert(count % 2 == 0);
+
+  va_list list;
+  va_start(list, count);
+  VALUE result = rb_hash_new();
+
+  for (int i = 0; i < count; i += 2) {
+    VALUE key = va_arg(list, VALUE);
+    VALUE value = va_arg(list, VALUE);
+    rb_hash_aset(result, key, value);
+  }
+
+  return result;
 }
 
 RBJIT_NAMESPACE_END
