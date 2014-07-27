18e6bfc7985fb14980ca00befdd6ef40eef19f6a
msumimz <msumimz@users.noreply.github.com>
2014-07-27 12:09:27 +0900

    Fix a constant definition bug in test/test_constant3.rb

--- rbjit/test/test_constant3.rb
+++ rbjit/test/test_constant3.rb
@@ -1,7 +1,11 @@
 load File.expand_path("assertions.rb", File.dirname(__FILE__))
 
+X = 999
+
 class C
-  remove_const :X if defined? X
+  if C.constants(false).include?(:X)
+    remove_const :X
+  end
   autoload :X, "xxx"
 
   def m
